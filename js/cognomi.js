c6051b06b4361a7ec1a206cd2118df3167e5889a
Margherita <margherita.testi@gmail.com>
2020-08-26 11:26:25 +0200

    correzione

--- js/cognomi.js
+++ js/cognomi.js
@@ -1,15 +1,22 @@
+
+//definsco lista cognomi
+var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
 //Chiedere all’utente il cognome//
-var utente = prompt("Inserisci il tuo cognome");
-//inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Duzioni’, ‘Balsano’, ‘Verdi’//
-var lista = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
-var utenteInLista = lista.push(utente);
+var cognomeUtente = prompt("Inserisci il tuo cognome");
 
-//stampa la lista//
-for (var i=0; i < lista.length ; i++) {
-  var elenco = document.getElementById('Cognomi');
-  elenco = elenco + "<li>"+ lista[i] + utenteInLista + "</li>";
+//inserirlo il cogome nella lista//
+listaCognomi.push(cognomeUtente);
 
+//Ordino la lista alfabeticamente//
+listaCognomi.sort();
+
+//stampo la lista nell html//
+for ( i = 0; i < listaCognomi.length; i++) {
+  var lista = document.getElementById("lista").innerHTML;
+  lista = lista + "<li>" + listaCognomi[i] + "</li>";
+  document.getElementById("lista").innerHTML = lista;
 }
-//ordinata alfabeticamente//
-console.log(elenco);
-//scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova//
+//scrivo la posizione in lista//
+var posizione = listaCognomi.indexOf(cognomeUtente) +1;
+document.getElementById("posizione").innerHTML = "La tua posizione in lista è" + posizione;
